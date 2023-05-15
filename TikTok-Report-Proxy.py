82e3edd3caec3171f3bada6ea388066dd4a37a0e
IDA <132213924+LeetIDA@users.noreply.github.com>
2023-05-15 19:58:38 +0300

    Read Proxy From Text File (http/s)

---
+++ TikTok-Report-Proxy.py
@@ -0,0 +1,34 @@
+import time
+from datetime import datetime
+from colorama import *
+import requests
+from requests.sessions import session
+
+print(Fore.RED + '''
+████████╗██╗██╗  ██╗████████╗ ██████╗ ██╗  ██╗   ██████╗  █████╗ ███╗   ██╗
+╚══██╔══╝██║██║ ██╔╝╚══██╔══╝██╔═══██╗██║ ██╔╝   ██╔══██╗██╔══██╗████╗  ██║
+   ██║   ██║█████╔╝    ██║   ██║   ██║█████╔╝    ██████╔╝███████║██╔██╗ ██║
+   ██║   ██║██╔═██╗    ██║   ██║   ██║██╔═██╗    ██╔══██╗██╔══██║██║╚██╗██║
+   ██║   ██║██║  ██╗   ██║   ╚██████╔╝██║  ██╗██╗██████╔╝██║  ██║██║ ╚████║
+   ╚═╝   ╚═╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚═╝╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝
+                                         GitHub: LeetIDA                                  
+''')
+
+tiktok_url = input("Request Link: ")
+
+while True:
+    with open("proxy.txt", "r") as file:
+        proxies = file.read().splitlines()
+
+    for proxy in proxies:
+        try:
+            now = datetime.now()
+            current_time = now.strftime("%H:%M:%S")
+
+            req = session().post(tiktok_url, proxies={'http': f'http://{proxy}', 'https': f'http://{proxy}'})
+            print(Fore.RED + f'[{current_time}]' + Fore.GREEN + f' Proxy: {proxy} Report Sent')
+        except:
+            print(Fore.RED + 'Something went wrong. It may not be working properly.')
+            input('Press Enter to close the program')
+            exit()
+            
